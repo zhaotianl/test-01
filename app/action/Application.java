ed01cd7acff0c97023bec333feba830698305bd8
handong <handong@bus365.com>
2020-06-03 11:24:52 +0800

    提交不推送2

--- app/action/Application.java
+++ app/action/Application.java
@@ -3,6 +3,8 @@ package action;
 public class Application {
     // 一行测试注释
     public static void main(String[] args) {
+
+
         System.out.println("111111111111111111");
         System.out.println("111111111111111111");
         System.out.println("111111111111111111");
@@ -10,6 +12,18 @@ public class Application {
         System.out.println("111111111111111111");
         System.out.println("111111111111111111");
         System.out.println("111111111111111111");
+
+        System.out.println("000000000000000000");
+        System.out.println("000000000000000000");
+        System.out.println("000000000000000000");
+        System.out.println("000000000000000000");
+        System.out.println("000000000000000000");
+        System.out.println("000000000000000000");
+
+
+
+
+
 
 
     }
