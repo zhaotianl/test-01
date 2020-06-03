bb2cc0d793a71add9cfb7fd12130c368d5022037
handong <handong@bus365.com>
2020-06-03 11:28:09 +0800

    提交不推送3

--- app/action/Application.java
+++ app/action/Application.java
@@ -5,13 +5,6 @@ public class Application {
     public static void main(String[] args) {
 
 
-        System.out.println("111111111111111111");
-        System.out.println("111111111111111111");
-        System.out.println("111111111111111111");
-        System.out.println("111111111111111111");
-        System.out.println("111111111111111111");
-        System.out.println("111111111111111111");
-        System.out.println("111111111111111111");
 
         System.out.println("000000000000000000");
         System.out.println("000000000000000000");
