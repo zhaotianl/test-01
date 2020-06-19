b1ba8351ff34ad3aac662bcb54a19677b3535c40
zhaotianliang <zhaotianliang@bus365.com>
2020-06-19 17:19:34 +0800

    Application

--- app/action/Application.java
+++ app/action/Application.java
@@ -5,7 +5,6 @@ public class Application {
     public static void main(String[] args) {
 
 
-
         System.out.println("000000000000000000");
         System.out.println("000000000000000000");
         System.out.println("000000000000000000");
