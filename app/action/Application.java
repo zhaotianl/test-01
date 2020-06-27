d7334124c8d303c41e84e3f0c0caef888506f23c
zhaotianliang <zhaotianliang@bus365.com>
2020-06-27 14:13:38 +0800

    dddd

--- app/action/Application.java
+++ app/action/Application.java
@@ -1,5 +1,9 @@
 package action;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Application {
     // 一行测试注释
     public static void main(String[] args) {
@@ -11,10 +15,14 @@ public class Application {
         System.out.println("000000000000000000");
         System.out.println("000000000000000000");
         System.out.println("000000000000000000");
-
-
-
-
+        Map map = new HashMap();
+        map.put("1","");
+        String  aa = map.get("2")+"";
+        System.out.println(aa.equals("null"));
+        String  bb = (String) map.get("2");
+        System.out.println(bb.equals("n"));
+        System.out.println( map.get("2")+"");
+       System.out.println(map.get("1").toString());
 
 
 
